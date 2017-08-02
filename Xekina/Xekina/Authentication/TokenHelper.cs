7a30dc545aee4d22ea0eaf850198cb41ec6ef189
nick Hill <nick-9979@outlook.com>
2017-08-02 17:42:44 +0100

    General refactoring before holiday: trim down the external settings file (can still remove a bit more).  Next need to 1) move anything security related into key vault - make the right number of calls to keyvault (or cache) and 2) deploys automatically now, but doesnt have database and connection strings set up so it crashes when you hit the production url.

--- Xekina/Xekina/Authentication/TokenHelper.cs
+++ Xekina/Xekina/Authentication/TokenHelper.cs
@@ -16,8 +16,8 @@ namespace Xekina.Authentication
         public static async Task<string> GetTokenForCurrentApplication(string authority, string resource, string scope)
         {
             var authContext = new AuthenticationContext(authority);
-            ClientCredential clientCred = new ClientCredential(CloudConfigurationManager.GetSetting("ClientId"),
-                        CloudConfigurationManager.GetSetting("ClientSecret"));
+            ClientCredential clientCred = new ClientCredential(CloudConfigurationManager.GetSetting("ida:ClientId"),
+                        CloudConfigurationManager.GetSetting("ida:ClientSecret"));
             AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
 
             if (result == null)
@@ -29,18 +29,17 @@ namespace Xekina.Authentication
         {
             string clientId = CloudConfigurationManager.GetSetting("ida:ClientId");
             string clientSecret = CloudConfigurationManager.GetSetting("ida:ClientSecret");
-            string tenantId = CloudConfigurationManager.GetSetting("ida:TenantId");
             string aadInstance = CloudConfigurationManager.GetSetting("ida:AADInstance");
             string signedInUserID = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
             string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
             string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
-            string azureResourceManager = @"https://management.azure.com/";
+            string azureResourceManagerManagementUri = @"https://management.azure.com/";
 
             // get a token for the Graph without triggering any user interaction (from the cache, via multi-resource refresh token, etc)
             ClientCredential clientcred = new ClientCredential(clientId, clientSecret);
             // initialize AuthenticationContext with the token cache of the currently signed in user, as kept in the app's database
             AuthenticationContext authenticationContext = new AuthenticationContext(aadInstance + tenantID, new ADALTokenCache(signedInUserID));
-            AuthenticationResult authenticationResult = authenticationContext.AcquireToken(azureResourceManager, clientcred);
+            AuthenticationResult authenticationResult = authenticationContext.AcquireToken(azureResourceManagerManagementUri, clientcred);
             return authenticationResult.AccessToken;
         }
     }
