498353b762c9dd6a74caceff1364ce1763de6c94
nick Hill <nick-9979@outlook.com>
2017-08-18 16:23:27 +0100

    try to remove the spike submodule

---
+++ Spike/VSTS-Spike/Models/SampleSolution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xekina.Models
+{
+    public class SampleSolution
+    {
+        public string SolutionName { get; set; }
+    }
+}
---
+++ Spike/VSTS-Spike/Models/XekinaTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xekina.Models
+{
+    public class XekinaTemplate
+    {
+        public string TemplateName { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectStrapline { get; set; }
+        public bool CreateVSTSProject { get; set; }
+        public bool CreateDevTestLab { get; set; }
+        public bool CreateEnvironments { get; set; }
+        public bool CreateBuildAndReleaseProcess { get; set; }
+        public bool CommitSampleProject { get; set; }
+        public DateTimeOffset ProjectStartDate { get; set; }
+        public SampleSolution SampleSolution { get; set; }
+
+        public XekinaTemplate()
+        {
+            CreateVSTSProject = true;
+            CreateBuildAndReleaseProcess = false;
+            CreateEnvironments = false;
+            CreateDevTestLab = false;
+            CommitSampleProject = true;
+            ProjectStartDate = DateTime.Now;
+        }
+    }
+}
