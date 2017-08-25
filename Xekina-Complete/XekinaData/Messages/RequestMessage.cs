25fbb9fd508176a0eb03379d01afae8e5fcda070
nick Hill <nick-9979@outlook.com>
2017-08-25 12:40:29 +0100

    re-coded xekinadata in a new project

---
+++ Xekina-Complete/XekinaData/Messages/RequestMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xekina.Data.Models;
+
+namespace Xekina.Data.Messages
+{
+    public class RequestMessage
+    {
+
+        public int RequestID { get; set; }
+        
+        public static explicit operator RequestMessage(Request req)
+        {
+            return new RequestMessage
+            {
+                RequestID = req.RequestID
+            };
+        }
+    }
+}
+
+  
+
+    
+
---
+++ Xekina-Complete/XekinaData/Migrations/201708181618525_githubpatinuserdefaults.cs
@@ -0,0 +1,18 @@
+namespace Xekina.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class githubpatinuserdefaults : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.UserDefaults", "GitHubPersonalAccessToken", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.UserDefaults", "GitHubPersonalAccessToken");
+        }
+    }
+}
