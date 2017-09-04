363e0e90bff8ca8f922dfbb4c220411be32f92a9
nick Hill <nick-9979@outlook.com>
2017-09-04 12:39:11 +0100

    set automigration to allow data loss

--- Xekina-Complete/XekinaData/Migrations/Configuration.cs
+++ Xekina-Complete/XekinaData/Migrations/Configuration.cs
@@ -11,6 +11,7 @@ namespace Xekina.Data.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Xekina.Data.XekinaWebContext context)
